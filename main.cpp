74eb1c06a538d7175b7aaeaa02244d499121e592
cowchPotato <ivanjchen129@gmail.com>
2026-09-18 14:32:02 -0700

    Desktop icon

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     // Set the application icon.
     a.setWindowIcon(QIcon(":/marktek.png"));
+    a.setDesktopFileName("marktek");
     MainWindow w;
     w.show();
     return QApplication::exec();
