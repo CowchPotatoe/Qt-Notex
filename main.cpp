dde9158dcc6a461470c4749b9df9e8bb7e76340c
cowchPotato <ivanjchen129@gmail.com>
2026-09-18 01:03:15 -0700

    Attempting to create project icon

--- main.cpp
+++ main.cpp
@@ -1,10 +1,13 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QIcon>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    // Set the application icon.
+    a.setWindowIcon(QIcon(":/marktek.png"));
     MainWindow w;
     w.show();
     return QApplication::exec();
